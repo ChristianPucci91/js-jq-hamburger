a96a11d2c9c84e65f935a1a2f8411ce1718c0413
ChristianPucci91 <pucci.christian91@gmail.com>
2020-11-02 15:08:22 +0100

    Esercizio completo, update: create variabili.

--- js/main.js
+++ js/main.js
@@ -2,13 +2,24 @@
 // mostrare / nascondere il menu principale
 // (ricordate che per vedere l’hamburger in alto a destra dovete scendere a risoluzioni tipo tablet o mobile).
 
+
+// >>>>>>>>>>>>>>>>>> INIZIO ESERCIZIO <<<<<<<<<<<<<<<<<<<<
+
 // creo una funzione click sul selettore tag "a" del menu burger
 // quando clicco sull'icona si aprirà il menù con selettore .hamburger-menu a cui diamo una funzione show.
-$('.header-right > a').click(function() {
+
+var burgMenu = $('.header-right > a');
+
+burgMenu.click(function() {
   $('.hamburger-menu').show();
 });
 
 // Una volta che il menù è aperto applico la stessa funzione click sull'icona "X" del tag "a" con selettore .close e darò una funzione hide una volta cliccato
-$('.hamburger-menu a.close').click(function() {
+
+var xMenu = $('.hamburger-menu a.close');
+
+xMenu.click(function() {
   $('.hamburger-menu').hide();
 });
+
+// >>>>>>>>>>>>>>>>>> FINE ESERCIZIO <<<<<<<<<<<<<<<<<<<<
